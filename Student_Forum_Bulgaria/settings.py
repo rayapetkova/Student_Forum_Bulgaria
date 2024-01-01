5af6c315e869f87b6b6de3cfb32d5c4539760d0b
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-01-01 19:00:39 +0200

    Update settings.py file
    - Added ALLOWED_HOSTS
    - Added the new app 'accounts_users' to INSTALLED_APPS
    - Added STATICFILES_DIRS --> this is the directory in which I will hold the static files. When I'm using 'static' in the html files, I will be in the 'staticfiles' dir automatically and I will access the content in there

--- Student_Forum_Bulgaria/settings.py
+++ Student_Forum_Bulgaria/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-c)sp)zcbo&0w%dhl99##8rk08ne2^ap^@3k1s5*&4#697_zlfv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts_users'
 ]
 
 MIDDLEWARE = [
@@ -117,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'staticfiles',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
