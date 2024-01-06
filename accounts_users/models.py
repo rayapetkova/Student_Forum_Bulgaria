4e5ede5e4fd3fa22548d7c3474c34e773361e6b1
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-01-06 20:38:41 +0200

    Users management
    - Added two models - one for the user's email and password and one for the user's personal information (with a relation to the first model)
    - Changed the value of AUTH_USER_MODEL to "accounts_users.AppUser" (this is the main model that Django will be using for my application instead of its built-in one)

--- accounts_users/models.py
+++ accounts_users/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth import models as auth_models
+from django.core import validators
 from django.db import models
 
-# Create your models here.
+from accounts_users.managers import AppUserCustomManager
+
+
+# class Test(auth_models.AbstractUser):
+#     pass
+
+
+class AppUser(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
+
+    email = models.EmailField(
+        unique=True,
+        null=False,
+        blank=False
+    )
+
+    is_staff = models.BooleanField(
+        default=False
+    )
+
+    USERNAME_FIELD = "email"
+
+    objects = AppUserCustomManager()
+
+
+class ProfileUser(models.Model):
+
+    first_name = models.CharField(
+        max_length=150,
+        null=False,
+        blank=False,
+        validators=[
+            validators.MinLengthValidator(2, message="Your first name needs to consist of at least 2 letters!")
+        ]
+    )
+
+    last_name = models.CharField(
+        max_length=150,
+        null=False,
+        blank=False,
+        validators=[
+            validators.MinLengthValidator(2, message="Your last name needs to consist of at least 2 letters!")
+        ]
+    )
+
+    user = models.OneToOneField(
+        to="AppUser",
+        on_delete=models.CASCADE
+    )
