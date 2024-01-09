5c221f9221537084069c37c44d035383747e02d8
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-01-09 21:00:58 +0200

    Changed url for the login
    - Added the new login logic - the new class-based view 'LoginUser'

--- accounts_users/urls.py
+++ accounts_users/urls.py
@@ -4,6 +4,6 @@ from accounts_users import views
 
 urlpatterns = (
     path('register/', views.register_user, name='register-user'),
-    path('login/', views.login_user, name='login-user'),
+    path('login/', views.LoginUser.as_view(), name='login-user'),
     path('main_page/', views.main_view, name='main-page')
 )
