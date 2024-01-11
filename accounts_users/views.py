5d91c499f768b7a4b632c0e7bc0bebdce51a27ed
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-01-11 23:48:29 +0200

    Added new class-based view for register
    - Deleted the function-based view for register
    - Added a class-based view RegisterUser
    - Added a method "form_valid" - when the user registers successfully he will be logged in automatically

--- accounts_users/views.py
+++ accounts_users/views.py
@@ -1,13 +1,25 @@
-from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views, login
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from accounts_users.forms import RegisterUserForm
 
 
 class LoginUser(auth_views.LoginView):
     template_name = 'accounts/login_page.html'
 
 
-def register_user(request):
-    return render(request, 'accounts/register_page.html')
+class RegisterUser(CreateView):
+    template_name = 'accounts/register_page.html'
+    form_class = RegisterUserForm
+    success_url = reverse_lazy('main-page')
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+
+        login(self.request, self.object)
+        return result
 
 
 def login_user(request):
