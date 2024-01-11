5cf6ccb76350d727fe599c5837082595adbacde8
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-01-11 23:58:01 +0200

    Changed url for register

--- accounts_users/urls.py
+++ accounts_users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from accounts_users import views
 
 urlpatterns = (
-    path('register/', views.register_user, name='register-user'),
+    path('register/', views.RegisterUser.as_view(), name='register-user'),
     path('login/', views.LoginUser.as_view(), name='login-user'),
     path('main_page/', views.main_view, name='main-page')
 )
