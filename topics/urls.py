36ca38a95e17f856870ed44d43ce4bbfb5761ea2
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-01-19 16:52:02 +0200

    Added url for the new page - topic with comments

--- topics/urls.py
+++ topics/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from topics.views import SubjectsListView
+from topics.views import SubjectsListView, subject_topics
 
 urlpatterns = (
     path('subjects/', SubjectsListView.as_view(), name='subjects-list'),
+    path('subject_topics/', subject_topics, name='subject-topics')
 )
