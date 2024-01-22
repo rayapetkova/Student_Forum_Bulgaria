6b2b1923c084dfe47782ef860459a8bef9a5f705
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-01-22 16:57:51 +0200

    Created two new urls
    - Added url for the topics of a subject
    - Added url for the comments of a topic

--- topics/urls.py
+++ topics/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from topics.views import SubjectsListView, subject_topics
+from topics.views import SubjectsListView, subject_topics, topic_comments
 
 urlpatterns = (
     path('subjects/', SubjectsListView.as_view(), name='subjects-list'),
-    path('subject_topics/', subject_topics, name='subject-topics')
+    path('subject_topics/<int:subject_id>', subject_topics, name='subject-topics'),
+    path('topic_comments/<int:topic_id>', topic_comments, name='topic-comments')
 )
