4835e6e4dd36ce57996d1dca06d598679a4b9db9
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-01-22 16:57:17 +0200

    Created two funtion-based views
    - Created a view 'subject_topics' which renders the topics for one subjects. We have a context added.
    - Created a view 'topic_comments' which shows all the comments for a topic. We render an html file and give a context.

--- topics/views.py
+++ topics/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from topics.models import Subject
+from topics.models import Subject, Topic, Comment
 
 
 class SubjectsListView(ListView):
@@ -9,5 +9,30 @@ class SubjectsListView(ListView):
     template_name = 'main_pages/after_login_main_page.html'
 
 
-def subject_topics(request):
-    return render(request, 'main_pages/subject_topics.html')
+def subject_topics(request, subject_id):
+
+    current_subject_topics = Topic.objects.filter(subject_id=subject_id)
+
+    context = {
+        'topics': current_subject_topics,
+    }
+
+    for topic in current_subject_topics:
+        print(topic.user.profileuser.first_name, topic.user.profileuser.last_name)
+
+    return render(request, 'main_pages/subject_topics.html', context=context)
+
+
+def topic_comments(request, topic_id):
+
+    curr_topic = Topic.objects.get(id=topic_id)
+    current_topic_comments = Comment.objects.filter(topic_id=topic_id)
+
+    context = {
+        'topic': curr_topic,
+        'comments': current_topic_comments,
+    }
+
+    print(curr_topic.user.profileuser.first_name, curr_topic.user.profileuser.last_name)
+
+    return render(request, 'main_pages/topics_comments_page.html', context=context)
