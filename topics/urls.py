3b703feac257ec1e2e5490bed5050fc256b367f0
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-01-26 17:35:28 +0200

    Added new url to the new view (main_page)

--- topics/urls.py
+++ topics/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from topics.views import SubjectsListView, subject_topics, topic_comments
+from topics.views import SubjectsListView, subject_topics, topic_comments, main_page
 
 urlpatterns = (
+    path('', main_page, name='main-page'),
     path('subjects/', SubjectsListView.as_view(), name='subjects-list'),
     path('subject_topics/<int:subject_id>', subject_topics, name='subject-topics'),
     path('topic_comments/<int:topic_id>', topic_comments, name='topic-comments')
