b7f910bf705f371d2b6783292e1b509ea24f0c9f
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-01-31 00:00:05 +0200

    Added new url for the new view 'create new comment'

--- topics/urls.py
+++ topics/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from topics.views import SubjectsListView, subject_topics, topic_comments, main_page, CreateNewTopic
+from topics.views import SubjectsListView, subject_topics, topic_comments, main_page, CreateNewTopic, CreateNewComment
 
 urlpatterns = (
     path('', main_page, name='main-page'),
@@ -8,4 +8,5 @@ urlpatterns = (
     path('subject_topics/<int:subject_id>/', subject_topics, name='subject-topics'),
     path('add_new_topic/<int:subject_id>/<int:user_id>/', CreateNewTopic.as_view(), name='add-new-topic'),
     path('topic_comments/<int:topic_id>/', topic_comments, name='topic-comments'),
+    path('add_new_comment/<int:topic_id>/<int:user_id>/', CreateNewComment.as_view(), name='add-new-comment')
 )
