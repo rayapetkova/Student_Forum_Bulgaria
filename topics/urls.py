e01479ed9b5b9be39f3639ae770218cd5e88b5cd
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-01-30 22:30:43 +0200

    Shuffled urls.py
    - Moved the url for 'adding topics' before the url for 'topic comments'

--- topics/urls.py
+++ topics/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
 from topics.views import SubjectsListView, subject_topics, topic_comments, main_page, CreateNewTopic
 
 urlpatterns = (
     path('', main_page, name='main-page'),
     path('subjects/', SubjectsListView.as_view(), name='subjects-list'),
-    path('subject_topics/<int:subject_id>', subject_topics, name='subject-topics'),
-    path('topic_comments/<int:topic_id>', topic_comments, name='topic-comments'),
-    path('add_new_topic/<int:subject_id>/<int:user_id>', CreateNewTopic.as_view(), name='add-new-topic')
+    path('subject_topics/<int:subject_id>/', subject_topics, name='subject-topics'),
+    path('add_new_topic/<int:subject_id>/<int:user_id>/', CreateNewTopic.as_view(), name='add-new-topic'),
+    path('topic_comments/<int:topic_id>/', topic_comments, name='topic-comments'),
 )
