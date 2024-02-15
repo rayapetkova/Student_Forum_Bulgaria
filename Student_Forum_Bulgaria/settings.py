08b99343cf3d66f453a776efabff8e08f4654cd1
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-02-15 21:36:09 +0200

    Added STATIC_ROOT to settings

--- Student_Forum_Bulgaria/settings.py
+++ Student_Forum_Bulgaria/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, "collected-static")
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
