f20a1990f4dfba2d1b73067dc167e3f131bc3fb0
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-02-16 15:11:40 +0200

    Added admin

--- accounts_users/admin.py
+++ accounts_users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from accounts_users.models import AppUser, ProfileUser
+
+
 # Register your models here.
+@admin.register(AppUser)
+class AppUserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProfileUser)
+class ProfileUserAdmin(admin.ModelAdmin):
+    pass
--- topics/admin.py
+++ topics/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from topics.models import Subject, Topic, Comment
+
+
 # Register your models here.
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
