10bbe6815bf132d42ca4c53e7a6012d8eb1b808b
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-02-16 08:58:57 +0200

    Update settings

--- Student_Forum_Bulgaria/settings.py
+++ Student_Forum_Bulgaria/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c)sp)zcbo&0w%dhl99##8rk08ne2^ap^@3k1s5*&4#697_zlfv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -130,8 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "collected-static")
-STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "collected-static")
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'staticfiles/'),
