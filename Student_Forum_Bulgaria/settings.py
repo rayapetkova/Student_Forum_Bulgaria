19ecfccf32116fc70089e7de7485ffe17c0bc307
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-02-16 08:34:04 +0200

    Update settings

--- Student_Forum_Bulgaria/settings.py
+++ Student_Forum_Bulgaria/settings.py
@@ -130,11 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "collected-static")
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles/'),
+    os.path.join(BASE_DIR), 'static'
 )
 
 # Default primary key field type
