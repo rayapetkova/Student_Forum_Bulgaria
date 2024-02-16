39df4be00f0457dfeedd37ccc402bcd953370f61
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-02-16 09:03:12 +0200

    Update settings

--- Student_Forum_Bulgaria/settings.py
+++ Student_Forum_Bulgaria/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "collected-static")
+STATIC_ROOT = os.path.join(BASE_DIR, "collected-static")
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
